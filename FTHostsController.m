beb1ce6ef788785a6b39bf640127bafaa4743939
Sam McDonald <sam@sammcd.com>
2010-01-12 13:27:17 -0500

    Starting the line seperated branch.

--- FTHostsController.m
+++ FTHostsController.m
@@ -35,143 +35,50 @@
 
 
 - (void)writeHostsToFile {
-    NSMutableString*    hostsFile;
-    NSString*           sectionStart;
-    NSString*           sectionEnd;
+    NSString*   hostsFile;
+    NSString*   sectionStart;
+    NSString*   sectionEnd;
+    NSArray*    hostsArray;
+    int         startLine  = -1;
+    int         stopLine = -1;
+    
+    hostsFile = [self readHostsFile];
     
     sectionStart = [NSString stringWithFormat:@"# %@ START",uniqueName];
-    sectionEnd = [NSString stringWithFormat:@"# %@ END",uniqueName];
+    sectionEnd = [NSString stringWIthFormat:@"# %@ END",uniqueName];
     
-    // Open file
-    hostsFile = [[NSMutableString alloc] initWithString:[self readHostsFile]];
+    // now break into lines.
+    hostsArray = [hostsFile componentsSeparatedByString:@"\n"];
     
-    
-    NSMutableString* newHosts = [[NSMutableString alloc] initWithCapacity:[hosts count] * 25];
-    for (FTHost* host in hosts) {
-        [newHosts appendString:[NSString stringWithFormat:@"%@ %@\n", host.ip, host.name]];
-    } 
-
-    // Check if we have already talked to this file.
-    NSRange startRange = [hostsFile rangeOfString:sectionStart];  
-    
-    if ( startRange.location == NSNotFound ) {
-        // Did not find the section so create it.
-
-        [hostsFile appendString:[NSString stringWithFormat:@"%@\n",sectionStart]];
-        [hostsFile appendString:newHosts];
-        [hostsFile appendString:[NSString stringWithFormat:@"%@\n",sectionEnd]];
-
-    } else {
-        // We did find the section, so update it.
-        NSLog(@"Update");
-        NSRange rangeToDelete;
-        rangeToDelete.location = startRange.location + startRange.length; // end of first line
-        
-        NSRange endRange = [hostsFile rangeOfString:sectionEnd];
-        BOOL endRangeFound = YES;
-        if ( endRange.location == NSNotFound ) {
-            // Okay so the stupid user messed up my pretty little file.
-            // Time for some major repair!!
-            // todo: handle gracefully, right now we just delete it all
-            endRangeFound = NO;
-            rangeToDelete.length = [hostsFile length] - rangeToDelete.location - 1;
-            
-        } else {
-            rangeToDelete.length = endRange.location - startRange.location - 1;
-            NSLog(@"%d - %d = %d",endRange.location, startRange.location,rangeToDelete.length);
-
+    // Loop through the array looking for the line end and beginning.
+    int i;
+    for (i = 0; i < [hostsArray count]; i++ ) {
+        NSString* line = [hostsArray objectAtIndex:i];
+        if ( [line hasSuffix:sectionStart] ) {
+            sectionStart = i;
+            break;
         }
-        
-        NSLog(@"%d %d %d",rangeToDelete.location, rangeToDelete.length, [hostsFile length]);
-        
-        BOOL noHostsInSection;
-        if ( rangeToDelete.length > [sectionStart length] ) { 
-            [hostsFile deleteCharactersInRange:rangeToDelete];
-            noHostsInSection = YES;
-        }
-        
-        [hostsFile insertString:@"\n" atIndex:rangeToDelete.location];
-        [hostsFile insertString:newHosts atIndex:rangeToDelete.location+1];
-        [hostsFile insertString:@"\n" atIndex:rangeToDelete.location ];
-        
-        if ( !endRangeFound )
-            [hostsFile insertString:sectionEnd atIndex:rangeToDelete.location+ [newHosts length] ];
-                
     }
     
-
-    NSError* error;
-    [hostsFile writeToFile:@"/Users/sammcd/Desktop/hosts" atomically:YES encoding:NSASCIIStringEncoding error:&error];
-
+    for (i = 0; i < [hostsArray count]; i++ ) {
+        NSString* line = [hostsArray objectAtIndex:i];
+        if ( [line hasSuffix:sectionEnd ) {
+            sectionEnd = i;
+            break;
+        }
+    }
+              
     
-    // Flush dns caches 
-
-    [hostsFile release];
-    [newHosts release];
+    
 }
 
 - (void)removeHostsFromFile {
-    NSMutableString*     hostsFile;
-    NSString*           sectionStart;
-    NSString*           sectionEnd;
+    NSString*   hostsFile;
     
-    sectionStart = [NSString stringWithFormat:@"# %@ START",uniqueName];
-    sectionEnd = [NSString stringWithFormat:@"# %@ END",uniqueName];
+    hostsFile = [self readHostsFile];
     
-    // Open file
-    hostsFile = [[NSMutableString alloc] initWithString:[self readHostsFile]];
     
-    // Find section
-    // Check if we have already talked to this file.
-    NSRange startRange = [hostsFile rangeOfString:sectionStart];  
     
-    if ( startRange.location == NSNotFound ) {
-        // Did not find the section so do nothing.
-        
-    } else {
-        // We did find the section, so update it.
-        NSLog(@"Update");
-        NSRange rangeToDelete;
-        rangeToDelete.location = startRange.location + startRange.length; // end of first line
-        
-        NSRange endRange = [hostsFile rangeOfString:sectionEnd];
-        if ( endRange.location == NSNotFound ) {
-            // Okay so the stupid user messed up my pretty little file.
-            // Time for some major repair!!
-            // todo: handle gracefully, right now we just delete it all
-            
-            rangeToDelete.length = [hostsFile length] - rangeToDelete.location -1;
-            
-        } else {
-            rangeToDelete.length = endRange.location - startRange.location - 1;
-            NSLog(@"%d - %d = %d",endRange.location, startRange.location,rangeToDelete.length);
-            
-        }
-        
-        NSLog(@"%d %d %d",rangeToDelete.location, rangeToDelete.length, [hostsFile length]);
-        
-        NSLog(@"%@",hostsFile);
-        BOOL noHostsInSection = NO;
-        if ( rangeToDelete.length > [sectionStart length] ) { 
-            [hostsFile deleteCharactersInRange:rangeToDelete];
-            noHostsInSection = YES;
-        }
-        
-            
-        NSLog(@"===================================");
-        NSLog(@"%@",hostsFile);
-        //NSLog(@"length: %d index: %d",[hostsFile length];
-        [hostsFile insertString:@"\n" atIndex:rangeToDelete.location ];
-        [hostsFile insertString:sectionEnd atIndex:rangeToDelete.location+1];
-        
-    }
-    
-    NSLog(@"Tried to delete");
-    
-    NSError* error;
-    [hostsFile writeToFile:@"/Users/sammcd/Desktop/hosts" atomically:YES encoding:NSASCIIStringEncoding error:&error];
-    
-    // Flush DNS caches
 }
 
 - (void)setUniqueName:(NSString*)aName {
